ce1d545b710640ff84078df643544af3437e1b18
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-09-19 18:07:46 +0530

    Setup Compose Viewport to render UI

--- wasm-app/src/wasmJsMain/kotlin/com/sudarshanmhasrup/minimalist/Main.kt
+++ wasm-app/src/wasmJsMain/kotlin/com/sudarshanmhasrup/minimalist/Main.kt
@@ -1,5 +1,12 @@
 package com.sudarshanmhasrup.minimalist
 
-fun main() {
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+import kotlinx.browser.document
 
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    ComposeViewport(viewportContainer = document.body!!) {
+
+    }
 }
