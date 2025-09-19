b3181120cdc2de48af51e5007cce431d2d3f6cd6
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-09-19 12:55:54 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+rootProject.name = "NA"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+    }
+}
