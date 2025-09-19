69685aa9fddf21be31d782c4addf9631bc809731
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-09-19 17:27:22 +0530

    Add google and maven central for plugin source

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,22 @@
 rootProject.name = "Minimalist"
 
+@Suppress("UnstableApiUsage")
 pluginManagement {
     repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
         gradlePluginPortal()
     }
 }
 
 @Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google {
             mavenContent {
