606000ca85b265bac494140a1a57d914925b05ef
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-09-19 14:49:51 +0530

    Add google as source of dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,13 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
         mavenCentral()
     }
 }
